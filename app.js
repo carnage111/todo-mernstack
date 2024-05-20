7959fc2664e09cfce2c636afe163432655dcffbb
carnage <121715018+carnage111@users.noreply.github.com>
2024-05-20 12:00:53 +0530

    Your commit message here

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const app = express()
 
 app.use(express.json())
 
+
+// /api/v1/todo is the base route
 app.use('/api/v1/todo', todoRoutes) //to use the todoRoutes, we are uisng the /api/v1/todo as a prefix to all the routes in todoRoutes
 
 export default app;
